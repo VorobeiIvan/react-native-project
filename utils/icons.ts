b28857a9b45567a4283c3aa9800c53ac2e51b41d
Ivan <0675361905vi@gmail.com>
2024-10-22 14:00:02 +0300

    add utilits icons and images

--- utils/icons.ts
+++ utils/icons.ts
@@ -1,6 +1,16 @@
 const icons = {
   add: require("../assets/icons/add.svg"),
   remove: require("../assets/icons/remove.svg"),
+  arrowLeft: require("../assets/icons/arrow-left.svg"),
+  camera: require("../assets/icons/camera.svg"),
+  grid: require("../assets/icons/grid.svg"),
+  logOut: require("../assets/icons/log-out.svg"),
+  mapPin: require("../assets/icons/map-pin.svg"),
+  messageCircle: require("../assets/icons/message-circle.svg"),
+  thumbsUp: require("../assets/icons/thumbs-up.svg"),
+  trash2: require("../assets/icons/trash-2.svg"),
+  union: require("../assets/icons/union.svg"),
+  user: require("../assets/icons/user.svg"),
 };
 
 export default icons;
--- utils/images.ts
+++ utils/images.ts
@@ -3,6 +3,14 @@ const images = {
   girl: require("../assets/images/girl.png"),
   add: require("../assets/images/add.png"),
   avatar: require("../assets/images/avatar.png"),
+  remove: require("../assets/images/remove.png"),
+  sea: require("../assets/images/sea.png"),
+  house: require("../assets/images/house.png"),
+  forest: require("../assets/images/forest.png"),
+  icon: require("../assets/images/icon.png"),
+  splash: require("../assets/images/splash.png"),
+  favicon: require("../assets/images/favicon.png"),
+  adaptiveIcon: require("../assets/images/adaptive-icon.png"),
 };
 
 export default images;
